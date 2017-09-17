60f50ca17e7a3deaf7e69b082d0052e4cc953a2a
Beej <beej126@hotmail.com>
2017-09-16 17:26:45 -0700

    support tilde (~) tab completion
    from here: https://github.com/cmderdev/cmder/issues/1051#issuecomment-242201036

--- clink/lua/clink.lua
+++ clink/lua/clink.lua
@@ -406,3 +406,20 @@ function clink.filter_prompt(prompt)
 end
 
 -- vim: expandtab
+
+-- from here: https://github.com/cmderdev/cmder/issues/1051#issuecomment-242201036
+local function tilde_match (text, f, l)
+    if text == '~' then
+        clink.add_match(clink.get_env('userprofile'))
+        clink.matches_are_files()
+        return true
+    end
+    if text:sub(1, 1) == '~' then
+        clink.add_match(string.gsub(text, "~", clink.get_env('userprofile'), 1))
+        -- second match prevents adding a space so we can look for more matches
+        clink.add_match(string.gsub(text, "~", clink.get_env('userprofile'), 1) .. '+')
+        clink.matches_are_files()
+        return true
+    end
+end
+clink.register_match_generator(tilde_match, 1)
