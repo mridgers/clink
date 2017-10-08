fd0284fe9bba73c2e4eb174d7142a1d3ae78997c
Beej <beej126@hotmail.com>
2017-10-07 17:42:49 -0700

    fix for ~ completion with clink-menu-completion-shim
    better approach to allowing for second TAB press to complete remainder of line, previous approach was appending unwanted "+" when clink_inputrc_base was set for "\t": clink-menu-completion-shim

--- clink/lua/clink.lua
+++ clink/lua/clink.lua
@@ -416,8 +416,7 @@ local function tilde_match (text, f, l)
     end
     if text:sub(1, 1) == '~' then
         clink.add_match(string.gsub(text, "~", clink.get_env('userprofile'), 1))
-        -- second match prevents adding a space so we can look for more matches
-        clink.add_match(string.gsub(text, "~", clink.get_env('userprofile'), 1) .. '+')
+        clink.suppress_char_append()
         clink.matches_are_files()
         return true
     end
